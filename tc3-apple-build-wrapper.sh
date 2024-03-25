4e9735c9c50cc6d802c5a9606383bc0a64ebc0e9
Andy Bruce <andybrucenet@gmail.com>
2024-03-25 09:54:30 -0400

    Updates

--- tc3-apple-build-wrapper.sh
+++ tc3-apple-build-wrapper.sh
@@ -3,44 +3,45 @@
 #
 # Build openssl for apple with mods to compile correctly
 the_log=/tmp/build_openssl_apple.log
-echo 'build_openssl_apple: Start...' | tee "$the_log"
-echo '' | tee -a "$the_log"
+echo 'build_openssl_apple: Start...'
+echo ''
 # no more auto-remove...
-#echo 'remove temporary files...' | tee "$the_log"
-#echo "rm -fR ./iphoneos/* ./iphonesimulator/* ./macosx/* ./macosx_catalyst/*" | tee -a "$the_log"
-#rm -fR ./iphoneos/* ./iphonesimulator/* ./macosx/* ./macosx_catalyst/* 2>&1 | tee -a "$the_log"
-#echo '' | tee -a "$the_log"
-echo "scripts/build.sh" | tee -a "$the_log"
-scripts/build.sh 2>&1 | tee -a "$the_log"
+#echo 'remove temporary files...'
+#echo "rm -fR ./iphoneos/* ./iphonesimulator/* ./macosx/* ./macosx_catalyst/*"
+#rm -fR ./iphoneos/* ./iphonesimulator/* ./macosx/* ./macosx_catalyst/*
+#echo ''
+echo "scripts/build.sh"
+scripts/build.sh
 the_rc=$? ; [ $the_rc -ne 0 ] && exit $the_rc
-echo '' | tee -a "$the_log"
-echo 'Updating include files...' | tee -a "$the_log"
-for i in iphoneos iphonesimulator macosx macosx_catalyst ; do
-  echo "cd $i/include" | tee -a "$the_log"
-  cd $i/include 2>&1 | tee -a "$the_log"
+echo ''
+echo 'Updating include files...'
+#for i in iphoneos iphonesimulator macosx macosx_catalyst ; do
+for i in iphonesimulator ; do
+  echo "cd $i/include"
+  cd $i/include
   the_rc=$? ; [ $the_rc -ne 0 ] && continue
   if [ -d ./OpenSSL ] ; then
-    echo "mv OpenSSL openssl" | tee -a "$the_log"
-    mv OpenSSL openssl 2>&1 | tee -a "$the_log"
+    echo "mv OpenSSL openssl"
+    mv OpenSSL openssl
   fi
   if [ ! -d ./openssl ] ; then
     echo 'Missing openssl'
     cd ../..
     continue
   fi
-  echo "cd openssl" | tee -a "$the_log"
-  cd openssl 2>&1 | tee -a "$the_log"
+  echo "cd openssl"
+  cd openssl 2>&1
   the_rc=$?
   if [ $the_rc -eq 0 ] ; then
-    echo "sed -ie 's/<OpenSSL/<openssl/' *.h" | tee -a "$the_log"
-    sed -ie 's/<OpenSSL/<openssl/' *.h 2>&1 | tee -a "$the_log"
-    echo 'cd ../../..' | tee -a "$the_log"
-    cd ../../.. 2>&1 | tee -a "$the_log"
+    echo "sed -ie 's/<OpenSSL/<openssl/' *.h"
+    sed -ie 's/<OpenSSL/<openssl/' *.h
+    echo 'cd ../../..'
+    cd ../../..
   else
-    echo 'cd ../..' | tee -a "$the_log"
-    cd ../.. 2>&1 | tee -a "$the_log"
+    echo 'cd ../..'
+    cd ../..
   fi
-  echo '' | tee -a "$the_log"
+  echo ''
 done
-echo 'Done.' | tee -a "$the_log"
+echo 'Done.'
 
